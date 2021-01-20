9b10ec14f3115a0fe9a7c2bed1e745d2fde4c7b3
Alex <alexss81@mail.ru>
2021-01-20 23:30:06 +0300

    comment

--- my_OWM.py
+++ my_OWM.py
@@ -1,36 +1,35 @@
+import pyowm
 from pyowm import OWM
-#from pyowm.utils import timestamps
 from pyowm.utils.config import get_default_config
+import configparser
+
 
 
 config_dict = get_default_config()
-#config_dict = config.get_default_config_for_subscription_type('professional')
 config_dict['language'] = 'ru'  # your language here, eg. Russian
-owm = OWM('api_key', config_dict)
-
+owm = OWM('key',config_dict)
 
 city = input('Введите название города:')
-
 mgr = owm.weather_manager()
 
 # Search for current weather in 'city' and get details
-observation = mgr.weather_at_place(city)
-w = observation.weather
-temperature = w.temperature('celsius')['temp']
-wind = w.wind()['speed']
+try:
+    observation = mgr.weather_at_place(city)
+    w = observation.weather
+    temp = w.temperature('celsius')["temp"]
+    wind = w.wind()['speed']
+    status = w.detailed_status
+    hum = w.humidity
+    cloud = w.clouds
 
-w.detailed_status         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity                # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain                    # {}
-w.heat_index              # None
-w.clouds                  # 75
+    print('В городе ' + city + ' температура сейчас: '+ str(temp) + ' по цельсию')
+    print('Также в городе: ' + status)
+    print('Скорость ветра: ' + str(wind) + ' м/с')
+    print('Относительная влажность: ' + str(hum) + ' %')
+    print('Скорость ветра: ' + str(cloud) + ' м/с')
 
+except (pyowm.commons.exceptions.NotFoundError):
+    print('Ошибка! Город не найден.')
 
-print('В городе ' + city + ' температура сейчас: '+ str(temperature) + ' по цельсию')
-print('Также в городе: ' + w.detailed_status)
-print('Скорость ветра: ' + str(wind) + ' м/с')
-print('Относительная влажность: ' + str(w.humidity) + ' %')
 
 
